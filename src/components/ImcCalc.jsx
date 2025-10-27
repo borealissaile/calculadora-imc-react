8ea98dce89c1146b4e2e919ba20d744bbde1a3e6
borealissaile <eliasfreitasbraga@gmail.com>
2025-10-27 11:27:46 -0300

    feat: Project Completed

--- src/components/ImcCalc.jsx
+++ src/components/ImcCalc.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import Button from "./Button";
 import "./ImcCalc.css";
 
-const ImcCalc = () => {
+const ImcCalc = ({ calcImc }) => {
   const [height, setHeight] = useState("");
   const [wight, setWeight] = useState("");
 
@@ -56,7 +56,11 @@ const ImcCalc = () => {
           </div>
         </div>
         <div className="action-control">
-          <Button id="calc-btn" text="Calcular" />
+          <Button
+            id="calc-btn"
+            text="Calcular"
+            action={(event) => calcImc(event, height, wight)}
+          />
           <Button id="clear-btn" text="Limpar" action={clearForm} />
         </div>
       </form>
